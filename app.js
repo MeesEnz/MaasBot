9438cd5ed0fa6b12de6ad2451d49c76cf7f319e3
MeesEnz <36102280+MeesEnz@users.noreply.github.com>
2018-05-25 23:38:41 +0200

    Update app.js

--- app.js
+++ app.js
@@ -32,24 +32,6 @@ client.on('message', message => {
     let args = message.content.split(" ").slice(1);
     let argsss = message.content.split(" ").slice(2);
 
-    if(msg.startsWith(prefix + "purge")) {
-      
-      const deleteCount = parseInt(args[0], 10);
-      
-      if(!deleteCount || deleteCount < 2 || deleteCount > 100)
-        return message.reply("Please provide a number between 2 and 100 for the number of messages to delete");
-      
-      const fetched = await message.channel.fetchMessages({count: deleteCount});
-      message.channel.bulkDelete(fetched)
-        .catch(error => message.reply(`Couldn't delete messages because of: ${error}`));
-    }
-
-
-  // !remind (replies with the arguments that have been said)
-  if (msg.startsWith(prefix + "remind")){ 
-    setTimeout('message', 2000);
-    message.channel.sendMessage(argsss.join(" "))
-  }else
 
 
   
