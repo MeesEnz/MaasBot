a6c4c95b5511de3ec519c81e864ab503beef2731
MeesEnz <36102280+MeesEnz@users.noreply.github.com>
2018-06-30 16:33:47 +0200

    Update app.js

--- app.js
+++ app.js
@@ -299,21 +299,12 @@ client.on('message', message => {
   // !embed
   if (message.content.startsWith(prefix + 'embed')) {
     if (message.member.hasPermission("ADMINISTRATOR")){
-      if (!args){
+      if (args){
       let said = args.join(" ");
       message.channel.send({embed: {
-          color: 3447003,
-          description: "Error: Missing Argument, please insert something to say.",
-          title: "Error"
-      }})}
-    else{
-      message.channel.send({embed: {
-        color: 3447003,
-        description: said,
-        title: "Embedded"
-
-      }})}
-    }
+          color: 25500,
+          description: said
+      }})}}
   else {
     message.channel.send('Sorry, You do not have Administrator Permissions')
   }
