80b8582187b71867908f0a69680e04e3cd85e28f
septictm <39461856+septictm@users.noreply.github.com>
2018-07-08 21:31:59 +0200

    Update app.js

--- app.js
+++ app.js
@@ -415,7 +415,7 @@ client.on('message', message => {
         .addField("Reason", reason);
 
         message.reply("Your report has been submitted.");
-        let reportschannel = message.guild.channels.find(`name`, "reports");
+        let reportschannel = message.guild.channels.find(`name`, "reportss");
         if(!reportschannel) return message.channel.send("couldn't find reports channel.");
 
         message.delete().catch(O_o=>{});
