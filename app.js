fc2b4573b60ef6b0a8d5da0432bd85a590c4290b
septictm <39461856+septictm@users.noreply.github.com>
2018-08-04 13:38:38 +0200

    Update app.js

--- app.js
+++ app.js
@@ -480,10 +480,17 @@ client.on('message', message => {
   }
   
     if (message.content.startsWith(prefix + "log")){
+          if(message.member.roles.some(r=>["Staff"].includes(r.name)) ) {
+      
           message.reply(`I sent you the log in private messages.`);
           message.author.send("`You told me to log this: `" + `\n` + message.content.slice(5));
           let logchannel = message.guild.channels.find(`name`, "log");
     logchannel.send("`" + message.author.username + " logged this: `" + "\n" + message.content.slice(5))
+          }
+       else{
+        message.reply("Sorry, you cannot log things")
+    }
+      
     }
 
  
