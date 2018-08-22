ab7c10f5ddc68f7a692c6dbede0293188ad57380
septictm <39461856+septictm@users.noreply.github.com>
2018-08-22 14:30:30 +0200

    Update app.js

--- app.js
+++ app.js
@@ -478,7 +478,7 @@ client.on('message', message => {
     message.reply("Done.")
     let logchannel = message.guild.channels.find(`name`, "log");
     logchannel.send("`" + message.author.username + " issued this warning: `" + '\n' + wUser + '\n' + wreason + '\n' + "`        `")
-   wUser.send(message.author.username + "warned you for: " + wreason)
+   wUser.send(message.author.username + " warned you for: " + wreason)
         
         }
 else{
